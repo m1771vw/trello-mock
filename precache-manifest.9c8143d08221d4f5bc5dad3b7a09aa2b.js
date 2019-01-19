5e935805c3ffd11e6f40af70858f019a94ca268a
William Yang <williamyang93@gmail.com>
2019-01-18 17:24:53 -0800

    Updates

---
+++ precache-manifest.9c8143d08221d4f5bc5dad3b7a09aa2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24ff89874c6fad77caeb",
+    "url": "/trello-mock/static/js/runtime~main.24ff8987.js"
+  },
+  {
+    "revision": "d66107d6cbd38dc7e647",
+    "url": "/trello-mock/static/js/main.d66107d6.chunk.js"
+  },
+  {
+    "revision": "198cb621ba56dae4a943",
+    "url": "/trello-mock/static/js/1.198cb621.chunk.js"
+  },
+  {
+    "revision": "d66107d6cbd38dc7e647",
+    "url": "/trello-mock/static/css/main.3b9f0796.chunk.css"
+  },
+  {
+    "revision": "5e518dc5810eac1fc8c59eb9194fff07",
+    "url": "/trello-mock/index.html"
+  }
+];
